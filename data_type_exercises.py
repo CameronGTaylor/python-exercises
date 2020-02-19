ee2f0b28d4faf3daac7df70c4fc4ab3ac8a6a506
Cameron Taylor <taylor.cameron.g@gmail.com>
2020-02-19 14:34:58 -0600

    Add list_comp_exercises

--- data_type_exercises.py
+++ data_type_exercises.py
@@ -1,16 +1,14 @@
 
 
 username = 'codeup'
-password = 'notastrongpassword'
+password = 'notastrongpassword '
 def checks(username, password):
     if (
     len(password) >= 5 and
     len(username) <= 20 and 
     password != username and
-    username[0] == username.strip()[0] and
-    password[0] == password.strip()[0] and 
-    username[-1] == username.strip()[-1] and
-    password[-1] == password.strip()[-1]) :
+    username == username.strip() and
+    password == password.strip()) :
         return "Success"
     else:
         return "Try a different username/password combo"
