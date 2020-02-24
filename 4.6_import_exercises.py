1aa0dabda39704d36c4ba6d9aacabb5d484bb372
Cameron Taylor <taylor.cameron.g@gmail.com>
2020-02-24 13:45:00 -0600

    Begin import_exercises and create copy of function_exercises

---
+++ 4.6_import_exercises.py
@@ -0,0 +1,14 @@
+#1a
+# import function_exercises
+# print(function_exercises.cumsum([1,3,5])
+#1b
+# from function_exercises import normalize_name
+# print(normalize_name('823 wa^nt_ some@b*ody&^ to_ love'))
+#1c
+# from function_exercises import capt as capitalize
+# print(capitalize('bob barker'))
+
+#1
+import itertools as it
+
+print(it.product('abc', '123'))
